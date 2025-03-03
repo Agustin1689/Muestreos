e006dfe551c16c656281683a27b0ee4432c34664
Agustín Gagliardi <agustin.gagliardi.castro@gmail.com>
2025-03-03 12:14:25 -0300

    ejecución de muestreos

--- Muestreos.R
+++ Muestreos.R
@@ -10,3 +10,40 @@ head(titanic)
 
 dim(titanic)
 
+
+# Tipos de muestreos
+
+## Muestreo aleatorio simple:
+
+# Muestreo aleatorio simple sin reposición: seleccionar el 50 % de los datos
+set.seed(123)
+Titanic_sample_sin_reemplazo = titanic %>% sample_frac(0.5)
+dim(Titanic_sample_sin_reemplazo)
+
+# Muestreo aleatorio simple con reposición: seleccionar el 50 % de los datos
+set.seed(123)
+Titanic_sample_con_reemplazo = titanic %>% sample_frac(0.5, replace = TRUE)
+dim(Titanic_sample_con_reemplazo)
+
+
+# Muestreo estrateficado
+
+# Crear un muestreo estratificado por clase de pasajero
+Titanic_estratificado = titanic %>%
+group_by(Pclass) %>%
+sample_frac(0.5)
+dim(Titanic_estratificado)
+
+
+# Crear una variable de conglomerado, por ejemplo, la variable 'Embarked' 
+# que indica el puerto de embarque
+# Realizar el muestreo por conglomerado seleccionando al azar uno 
+# de los puertos de embarque
+
+selected_port = sample(unique(titanic$Embarked), 1)
+
+# Filtrar los datos para incluir solo el puerto seleccionado
+
+Titanic_conglom= titanic %>%
+filter(Embarked == selected_port)
+dim(Titanic_conglom)
